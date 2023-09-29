f53aabdbfe622f3cec720f0fa376406496a03c32
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-09-29 16:01:21 +0500

    #fix #add_login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import "./Login.scss"
+import Helmet from "../Helmet/Helmet"
 
 export default function Login() {
     return (
         <React.Fragment>
-            <div className='header_login'>
-                
-
-                <div className="login_imgs">
-                    
+            <Helmet title="Login">
+                <div className="login">
+                    <div className='login-back-img'>
+                        <div className='login-section-div'>
+                            <h1>
+                                Login In
+                            </h1>
+                            <div className="login-section-div-inputs">
+                                <input type="text" />
+                                <br />
+                                <input type="password" />
+                            </div>
+                            <div className="login-section-div-btn">
+                                <button>Enter</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </Helmet>
         </React.Fragment>
     )
 }
