7584edfdf93a72fe0776ed6c51a84836ffc63060
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-10-10 17:36:21 +0500

    #fix #add filter and scroll top #CarItem and CarListing

--- src/components/UI/CarItem.jsx
+++ src/components/UI/CarItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col } from "reactstrap";
+import { Col, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../../styles/car-item.css";
 import axios from "axios";
@@ -11,6 +11,8 @@ const CarItem = (props) => {
   const [showMore, setShowMore] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [noMoreItems, setNoMoreItems] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [noCarsFound, setNoCarsFound] = useState(false); 
 
   useEffect(() => {
     loadCars();
@@ -22,9 +24,9 @@ const CarItem = (props) => {
       .get(`http://localhost:8080/cars?_page=${currentPage}&_limit=6`)
       .then((response) => {
         if (response.data.length === 0) {
+          alert("Cartlar qolmadi");
           setNoMoreItems(true);
           setShowMore(false);
-          alert("Cartlar qolmadi");
         } else {
           const updatedCars = [...initialCars, ...response.data];
           setCars(updatedCars);
@@ -40,6 +42,28 @@ const CarItem = (props) => {
       });
   };
 
+  const filterCarsByName = (input) => {
+    const filteredCars = initialCars.filter((car) =>
+      car.brand.toLowerCase().includes(input.toLowerCase())
+    );
+
+    if (filteredCars.length === 0) {
+      setNoCarsFound(true); 
+      setShowMore(false); 
+    } else {
+      setNoCarsFound(false); 
+      setShowMore(true); 
+    }
+
+    setCars(filteredCars);
+  };
+
+  const handleSearchInputChange = (event) => {
+    const inputValue = event.target.value;
+    setSearchInput(inputValue);
+    filterCarsByName(inputValue);
+  };
+
   const handleSeeMore = () => {
     loadCars();
   };
@@ -50,49 +74,61 @@ const CarItem = (props) => {
 
   return (
     <Col lg="4" md="4" sm="6" className="mb-5">
-      <div className="cars">
-        {cars.map((car) => (
-          <div className="car__item" key={car.id}>
-            <div className="car__img">
-              <img
-                className="w-100"
-                src={process.env.PUBLIC_URL + "/images/" + car.image}
-                alt="error"
-              />
-            </div>
-
-            <div className="car__item-content mt-4">
-              <h4 className="section__title text-center">{car.carName}</h4>
-              <h6 className="rent__price text-center mt-">
-                ${car.price}.00 <span>/ Day</span>
-              </h6>
-
-              <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
-                <span className=" d-flex align-items-center gap-1">
-                  <i className="ri-car-line"></i> {car.model}
-                </span>
-                <span className=" d-flex align-items-center gap-1">
-                  <i className="ri-settings-2-line"></i> {car.automatic}
-                </span>
-                <span className=" d-flex align-items-center gap-1">
-                  <i className="ri-timer-flash-line"></i> {car.speed}
-                </span>
-              </div>
-              <Link to={`/cars/${car.carName}`}>
-                <button className=" w-50 car__item-btn car__btn-rent">
-                  Rent
-                </button>
-              </Link>
-
-              <Link to={`/cars/${car.carName}`}>
-                <button className=" w-50 car__item-btn car__btn-details">
-                  Details
-                </button>
-              </Link>
-            </div>
-          </div>
-        ))}
+      <div className="car-search-input">
+        <Input
+          type="text"
+          placeholder="Search your Car Brand"
+          onChange={handleSearchInputChange}
+          value={searchInput}
+        />
       </div>
+      {noCarsFound ? (
+        <div className="car_filter_not_found">The car you wanted was not found</div>
+      ) : (
+        <div className="cars">
+          {cars.map((car) => (
+            <div className="car__item" key={car.id}>
+              <div className="car__img">
+                <img
+                  className="w-100"
+                  src={process.env.PUBLIC_URL + "/images/" + car.image}
+                  alt="error"
+                />
+              </div>
+
+              <div className="car__item-content mt-4">
+                <h4 className="section__title text-center">{car.carName}</h4>
+                <h6 className="rent__price text-center mt-">
+                  ${car.price}.00 <span>/ Day</span>
+                </h6>
+
+                <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
+                  <span className=" d-flex align-items-center gap-1">
+                    <i className="ri-car-line"></i> {car.model}
+                  </span>
+                  <span className=" d-flex align-items-center gap-1">
+                    <i className="ri-settings-2-line"></i> {car.automatic}
+                  </span>
+                  <span className=" d-flex align-items-center gap-1">
+                    <i className="ri-timer-flash-line"></i> {car.speed}
+                  </span>
+                </div>
+                <Link to={`/cars/${car.carName}`}>
+                  <button className=" w-50 car__item-btn car__btn-rent">
+                    Rent
+                  </button>
+                </Link>
+
+                <Link to={`/cars/${car.carName}`}>
+                  <button className=" w-50 car__item-btn car__btn-details">
+                    Details
+                  </button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
       {showMore && (
         <div className="btn_see_more_div">
           <button
--- src/pages/CarListing.jsx
+++ src/pages/CarListing.jsx
@@ -1,37 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import CarItem from "../components/UI/CarItem";
-import { Link } from "react-router-dom";
+// import {  } from "react-router-dom";
 // import CarItem from "../components/UI/CarItem";
 // import carData from "../assets/data/carData";
 
 const CarListing = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true); 
+      } else {
+        setIsVisible(false); 
+      }
+    };
+
+    return () => {
+      window.onscroll = null;
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', 
+    });
+  };
   return (
     <Helmet title="Cars">
       <CommonSection title="Car Listing" />
 
       {/* <section> */}
-        {/* <Container> */}
-          {/* <Row> */}
-            <Col lg="12" id="12">
-              <div id="sort" className=" d-flex align-items-center gap-3 mb-5">
-                <span className="SortBy d-flex align-items-center">
-                  <Link to={"#12"}><i className="ri-sort-asc"></i></Link>
-                </span>
+      {/* <Container> */}
+      {/* <Row> */}
+      <Col lg="12" id="12">
+        <div id="sort" className=" d-flex align-items-center gap-3 mb-5">
+          <span className="SortBy d-flex align-items-center" onClick={scrollToTop}>
+            <i className="ri-sort-asc"></i>
+          </span>
 
-                {/* <select>
+          {/* <select>
                   <option>Select</option>
                   <option value="low">Low to High</option>
                   <option value="high">High to Low</option>
                 </select> */}
-              </div>
-            </Col>
+        </div>
+      </Col>
 
-            <CarItem />
-          {/* </Row> */}
-        {/* </Container> */}
+      <CarItem />
+      {/* </Row> */}
+      {/* </Container> */}
       {/* </section> */}
     </Helmet>
   );
