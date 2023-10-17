e9f183fe466f03673dbbf88f2df17f9bdb5998e2
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-10-17 20:25:24 +0500

    #fix #add auth and user cabinet

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.scss';
 import Helmet from '../Helmet/Helmet';
 import CarSlider from '../UI/LoginSlider';
 import axios from 'axios';
 
-import img01 from "../../images/McLaren 1.png"
-import img02 from "../../images/Rolls Royce 1.png"
-import img03 from "../../images/audi 1.png"
-import img04 from "../../images/lambo 1.png"
-import img05 from "../../images/mers 1.png"
-import { Link } from 'react-router-dom';
+import img01 from "../../images/McLaren 1.png";
+import img02 from "../../images/Rolls Royce 1.png";
+import img03 from "../../images/audi 1.png";
+import img04 from "../../images/lambo 1.png";
+import img05 from "../../images/mers 1.png";
 
 export default function Login({ handleLogin }) {
     const [formData, setFormData] = useState({ name: '', password: '' });
     const [currentUser, setCurrentUser] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [rentedCars, setRentedCars] = useState([]);
+    
+    useEffect(() => {
+        if (localStorage.getItem('currentUser')) {
+            const user = JSON.parse(localStorage.getItem('currentUser'));
+            setCurrentUser(user);
+            setIsLoggedIn(true);
+        }
+    }, []);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -39,10 +48,19 @@ export default function Login({ handleLogin }) {
 
     const performLogin = (user) => {
         setCurrentUser(user);
+        setIsLoggedIn(true);
+        localStorage.setItem('currentUser', JSON.stringify(user));
         handleLogin(user);
     };
 
     const carImages = [img01, img02, img03, img04, img05];
+
+    const handleLogout = () => {
+        setCurrentUser(null);
+        setIsLoggedIn(false);
+        localStorage.removeItem('currentUser');
+        setRentedCars([]);
+    };
 
     return (
         <React.Fragment>
@@ -51,18 +69,19 @@ export default function Login({ handleLogin }) {
                 <div className="login-back-img">
                     <div className="login-div">
                         <div className="login-section-div">
-                            {currentUser ? (
+                            {isLoggedIn ? (
                                 <div className='login-current'>
                                     <h2>Tizimga xush kelibsiz, {currentUser.name}!</h2>
-                                    <Link to={"/"}><button onClick={() => setCurrentUser(null)}>Chiqish</button></Link>
+                                    <button onClick={handleLogout}>Log Out</button>
+                                    <div className="rented-cars">
+                                    </div>
                                 </div>
                             ) : (
                                 <form onSubmit={handleSubmit}>
                                     <h1>Login In</h1>
                                     <div className="login-section-div-inputs">
-                                        <input placeholder="Name" type="text" name="name" onChange={handleChange} />
-                                        <br />
-                                        <input placeholder="Password" type="password" name="password" onChange={handleChange} />
+                                        <input placeholder="Name" type="text" name="name" value={formData.name} onChange={handleChange} />
+                                        <input placeholder="Password" type="password" name="password" value={formData.password} onChange={handleChange} />
                                     </div>
                                     <div className="login-section-div-btn">
                                         <button type="submit">Enter</button>
@@ -70,7 +89,7 @@ export default function Login({ handleLogin }) {
                                 </form>
                             )}
                         </div>
-                            <CarSlider images={carImages.map((image, index) => ({ id: index, src: image }))} />
+                        <CarSlider images={carImages.map((image, index) => ({ id: index, src: image }))} />
                     </div>
                 </div>
             </div>
