b150b5312d852707bd6dbbe1b9f28f0035161a43
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-10-22 18:39:25 +0500

    #fix #fix user information #user

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from "react";
-import "./User.scss"
+import React, { useEffect, useState } from 'react';
+import './User.scss';
 import axios from 'axios';
-import CarDetails from "../../pages/CarDetails";
 
 export default function User({ user }) {
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    password: "",
+  const [userState, setUserState] = useState({
+    name: '',
+    email: '',
+    password: '',
   });
-  const [userCarHistory, setUserCarHistory] = useState([]);
-
   const [selectedCar, setSelectedCar] = useState(null);
+  const [userObject, setUserObject] = useState({});
 
   useEffect(() => {
     const fetchOtherUserData = async (userId) => {
       try {
         const response = await axios.get(`http://localhost:8080/user/${userId}`);
-        const data = response.data;
-        setUserData(data);
-        setUserCarHistory(data.userCarHistory || []);
+        setUserObject(response.data); 
       } catch (error) {
         console.error('Xato', error);
       }
@@ -27,6 +23,8 @@ export default function User({ user }) {
 
     if (user) {
       fetchOtherUserData(user.id);
+    } else {
+      setUserObject(JSON.parse(localStorage.getItem("currentUser")));
     }
   }, [user]);
 
@@ -37,23 +35,14 @@ export default function User({ user }) {
   return (
     <div className="user-container">
       <div>
-        <h2>Xush kelibsiz, {user ? user.name : 'Geust'}!</h2>
+        <h2>Xush kelibsiz, {user ? user.name : 'Guest'}!</h2>
         <div>
-          <p>Name: {userData.name}</p>
-          <p>Email: {userData.email}</p>
-          <p>Passwor: {userData.password}</p>
+          <p>Name: {userObject.name}</p>
+          <p>Email: {userObject.email}</p>
+          <p>Password: {userObject.password}</p>
         </div>
         <div>
           <h3>Mashina Tarixi:</h3>
-          <ul>
-            {userCarHistory.map((car, index) => (
-              <li key={index}>
-                Mashina nomi: {car.carName}, Marka: {car.brand}
-                <button onClick={() => showCarDetails(car.carName, car.brand)}>Tafsilotlarni ko'rsatish</button>
-                <CarDetails user={user} onReserve={showCarDetails} />
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </div>
